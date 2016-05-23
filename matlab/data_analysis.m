9e4b868baa4416507024f80bdf3092179e8ddbd9
Linnea Claesson <linnea47@gmail.com>
2016-05-23 15:36:55 +1000

    Added histogram of number of ratings, after normalisation using average user ratings.

--- matlab/data_analysis.m
+++ matlab/data_analysis.m
@@ -21,6 +21,12 @@ for i = 1:n_ratings
    data(m_index, u_index) = u(i,3);
 end
 
+% Min, max, mean and median rating - not very interesting?
+min_rat = nanmin(u(:,3))
+max_rat = nanmax(u(:,3))
+mean_rat = nanmean(u(:,3))
+meadian_rat = nanmedian(u(:,3))
+
 %%
 % Histogram of ratings
 hist = zeros(1,5);
@@ -36,7 +42,8 @@ title('100K ratings data set')
 
 %%
 % Normalisation of the data
-% Each missing rating is replaced by 3
+% Each missing rating is replaced by 3 (trivial with so many non-rated
+% movies
 data3 = data;
 data3(find(data3 ~= data3)) = 3; 
 
@@ -74,12 +81,37 @@ figure(3)
 bar(rat, hist_mav)
 xlabel('Ratings')
 ylabel('# of ratings')
-title('100K ratings data set, non-rated items set to average rating of item')
-
-% Z-score, mean of user/movie-rating
+title('Non-rated items set to average rating of item')
 
 %%
-% min, max, median, mean, Z-score
+% Normalisation using average ratings of user, rounded up to nearest
+% integer
+data_uav = data;
+for i = 1:n_users
+    [nrat junk] = size(find(data_uav(:,i)==data_uav(:,i)));
+    av = nansum(data_uav(:,i))/nrat;
+    data_uav(find(data_uav(:,i) ~= data_uav(:,i)),i) = av;
+end
+
+data_uav = ceil(data_uav);
+
+% Histogram of ratings
+hist_uav = zeros(1,5);
+for i = 1:5
+    [hist_uav(i) junk] = size(find(data_uav(:,:)==i));
+end
+
+figure(4)
+bar(rat, hist_uav)
+xlabel('Ratings')
+ylabel('# of ratings')
+title('Non-rated items set to average rating by user')
+
+%%
+% Normalisation using z-score, based on items. All non-rated items have
+% been set to the mean first
+dataz = data;
+dataz(find(isnan(dataz)))
 
 %%
 % Mean ratings of movies vs. number of movies
