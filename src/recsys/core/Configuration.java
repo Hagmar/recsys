46928e9cbbbcdd46f84dcfa97b03ff4468678589
Anton Jansson <antonajgarn@gmail.com>
2016-05-24 21:28:54 +1000

    30-nearest neighbor is better than k=5.
    Mean error for tested k:
    k=20 0.8015
    k=25 0.8004
    k=30 0.8004
    k=35 0.8005
    k=40 0.8007
    k=50 0.8012

--- src/recsys/core/Configuration.java
+++ src/recsys/core/Configuration.java
@@ -7,7 +7,7 @@ public class Configuration {
     /**
      * The number of nearest neighbors (k) to use for prediction.
      */
-    public static final int NEAREST_NEIGHBORS_NUMBER = 5;
+    public static final int NEAREST_NEIGHBORS_NUMBER = 30;
     /**
      * Use weighted average (based on user similarity) instead of the mean
      * of the similar users' ratings.
