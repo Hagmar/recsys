599a6c51bfbeb7b4c916326044169b5f1e7cdd20
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-05-27 12:22:36 +1000

    Implemented movie similarity function

---
+++ src/recsys/domain/MovieSimilarity.java
@@ -0,0 +1,50 @@
+package recsys.domain;
+
+import java.util.Map;
+
+/**
+ * Computes the similarity between two users.
+ */
+public class MovieSimilarity {
+
+    public double similarity(Movie m1, Movie m2) {
+        double yearSimilarity;
+        if (m1.getYear() == 0) {
+            yearSimilarity = 0;
+        } else {
+            // 0.0002 gives a reasonable similarity for zip codes in the USA
+            yearSimilarity = Math.abs(m1.getYear() - m2.getYear());
+            yearSimilarity = Math.exp(-0.2 * yearSimilarity);
+        }
+        double genreSimilarity = cosineSimilarity(m1, m2);
+        // TODO Weight genre and year similarity
+        return genreSimilarity;
+    }
+
+    private double cosineSimilarity(Movie m1, Movie m2) {
+        if (m1 == null || m2 == null)
+            return 0;
+
+        int scalarProduct = 0;
+        int length1 = 0, length2 = 0;   // ||u1||, ||u2|| length of rating vectors u1 and u2 (squared)
+        boolean[] m1genres = m1.getGenres();
+        boolean[] m2genres = m2.getGenres();
+        for (int i = 0; i < 19; i++) {
+            if (m1genres[i] == m2genres[i]) {
+                scalarProduct++;
+                length1++;
+                length2++;
+            } else if (m1genres[i]) {
+                length1++;
+            } else {
+                length2++;
+            }
+        }
+
+        // Not defined for zero lengths
+        if (length1 == 0 || length2 == 0)
+            return 0;
+
+        return scalarProduct / (Math.sqrt(length1) * Math.sqrt(length2));
+    }
+}
