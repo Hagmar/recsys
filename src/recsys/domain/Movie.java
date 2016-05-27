8b6d33f5dd0f29ac5299d51594b65b8925f8d064
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-05-27 12:21:35 +1000

    Movie class

---
+++ src/recsys/domain/Movie.java
@@ -0,0 +1,49 @@
+package recsys.domain;
+
+import java.io.Serializable;
+
+/**
+ * @author Magnus Hagmar.
+ */
+public class Movie implements Serializable {
+    private final int id;
+    private final int year;
+    private boolean[] genres;
+
+    public Movie(int id) {
+        this(id, 0, new boolean[0]);
+    }
+
+    public Movie(int id, int year, boolean[] genres) {
+        this.id = id;
+        this.year = year;
+        this.genres = genres;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public boolean[] getGenres() {
+        return genres;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Movie movie = (Movie) o;
+
+        return id == movie.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+}
