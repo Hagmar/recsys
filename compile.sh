5deabcdcfad524cd16e1168babe2bccb91575ae8
Anton Jansson <antonajgarn@gmail.com>
2016-06-01 15:29:23 +1000

    The compile script now downloads the libraries from Maven, if necessary.

--- compile.sh
+++ compile.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
+weka="weka-stable-3.6.13.jar";
+sqlite="sqlite-jdbc-3.8.11.2.jar";
+weka_download="http://central.maven.org/maven2/nz/ac/waikato/cms/weka/weka-stable/3.6.13/weka-stable-3.6.13.jar";
+sqlite_download="http://central.maven.org/maven2/org/xerial/sqlite-jdbc/3.8.11.2/sqlite-jdbc-3.8.11.2.jar";
+
+mkdir -p libs;
+cd libs;
+# Download libraries
+if [ ! -f "$weka" ]; then
+    echo "Downloading weka library...";
+    wget "$weka_download";
+fi
+if [ ! -f "$sqlite" ]; then
+    echo "Downloading sqlite library...";
+    wget "$sqlite_download";
+fi
+cd ..;
+
+# Compile
 mkdir -p build;
 cd src;
-javac -classpath ../libs/weka.jar:../libs/sqlite-jdbc-3.8.11.2.jar recsys/*.java recsys/*/*.java -d ../
+javac -classpath "../libs/$weka:../libs/$sqlite" recsys/*.java recsys/*/*.java -d ../;
+echo "Compiled!";
--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-classpath=".:libs/weka.jar:libs/sqlite-jdbc-3.8.11.2.jar";
+weka="weka-stable-3.6.13.jar";
+sqlite="sqlite-jdbc-3.8.11.2.jar";
+classpath=".:libs/$weka:libs/$sqlite";
+
 if [ "$1" = "cli" ]; then
     java -cp $classpath recsys.Main
 else
